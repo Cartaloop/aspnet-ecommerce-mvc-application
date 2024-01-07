8fe141fe6341573092c0e9e419d0bead60650d7a
Lucas Rech <140991738+Cartaloop@users.noreply.github.com>
2024-01-07 03:17:56 -0300

    create all models

---
+++ Models/Actor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using eTicket.Models.Bases;
+
+namespace eTicket.Models
+{
+    public class Actor : Person
+    {
+        [Key]
+        public int Id { get; set; }
+        
+    }
+}
---
+++ Models/Bases/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace eTicket.Models.Bases
+{
+    public class Person
+    {
+        public string ProfilePicture { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
